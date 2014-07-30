37ad7970ca7c201423341dfc59e1db5c929618a1
codingninja62001 <rdoh62001@gmail.com>
2014-07-30 09:18:00 -0400

    added boot anim resolution

--- device/lge/f6mt/cm.mk
+++ device/lge/f6mt/cm.mk
@@ -1,15 +1,27 @@
-# f6mt vendor blobs
-$(call inherit-product-if-exists, device/lge/f6mt/f6mt-vendor-blobs.mk)
+## Specify phone tech before including full_phone
+$(call inherit-product, vendor/cm/config/gsm.mk)
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 960
+TARGET_SCREEN_WIDTH  := 540
+
+# Release name
+PRODUCT_RELEASE_NAME := f6
+
+# NFC
+$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
 
 # Inherit some common CM stuff.
 $(call inherit-product, vendor/cm/config/common_full_phone.mk)
 
-$(call inherit-product, device/lge/f6mt/f6mt.mk)
+# Inherit device configuration
+$(call inherit-product, device/lge/f6mt/device_f6mt.mk)
 
-#PRODUCT_BUILD_PROP_OVERRIDES +=
-
+## Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := f6mt
 PRODUCT_NAME := cm_f6mt
-PRODUCT_BRAND := LGE
-PRODUCT_MANUFACTURER := lge
-PRODUCT_MODEL := LGMS500
+PRODUCT_BRAND := MetroPCS
+PRODUCT_MODEL := LG-MS500
+PRODUCT_MANUFACTURER := LGE
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=f6mt
